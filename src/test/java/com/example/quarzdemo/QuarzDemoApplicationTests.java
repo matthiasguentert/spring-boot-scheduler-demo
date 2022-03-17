27b0f02f5b85dedde651dbe28871a40df88d2799
Matthias Güntert <matthias.guentert@gmail.com>
2022-03-17 16:39:55 +0100

    Initial commit

---
+++ src/test/java/com/example/quarzdemo/QuarzDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quarzdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuarzDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
